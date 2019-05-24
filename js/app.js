3dfa58cc457a4a5e2201b654aaff2f2c55d23720
Lyndon Panton <LyndonPanton@gmail.com>
2019-05-24 22:57:08 +0100

    Added logic for checking if user has local storage available
    Added local storage for input fields so the keep the same text on page refresh or close

--- js/app.js
+++ js/app.js
@@ -1,9 +1,50 @@
 (function() {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
-	let form = document.getElementById("form");
-	form.addEventListener("submit", function(event) {
-		event.preventDefault();
-		console.log(this.children[0].children[1].value, this.children[1].children[1].value);
-	});
+	if (localStorage) {
+		console.log("This browser/device does support the Web Storage API");
+		init();
+	} else {
+		console.log("This browser/device does not support the Web Storage API");
+	}
+
+	function init() {
+		let form = document.getElementById("form");
+		form.addEventListener("submit", function(event) {
+			event.preventDefault();
+			console.log(this.children[0].children[1].value, this.children[1].children[1].value);
+		});
+
+		// console.log(!!localStorage.getItem("inputName"));
+		// console.log(!!localStorage.getItem("inputAge"));
+
+		// Name Storage
+
+		let formName = document.getElementById("form-name");
+
+		if (localStorage.getItem("inputName")) {
+			formName.value = localStorage.getItem("inputName");
+		}
+
+		formName.addEventListener("input", function() {
+			localStorage.setItem("inputName", this.value);
+			console.log(localStorage.getItem("inputName"));
+		});
+
+		// Age Storage
+
+		let formAge = document.getElementById("form-age");
+
+		if (localStorage.getItem("inputAge")) {
+			formAge.value = localStorage.getItem("inputAge");
+		}
+
+		formAge.addEventListener("input", function() {
+			localStorage.setItem("inputAge", this.value);
+			console.log(localStorage.getItem("inputAge"));
+		});
+
+
+		
+	}
 }());
