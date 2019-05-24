90d1ae8009c34083fe27519de11340607a8a3002
Lyndon Panton <LyndonPanton@gmail.com>
2019-05-24 22:08:59 +0100

    Added a submit event listener to the form

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 (function() {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
+
+	let form = document.getElementById("form");
+	form.addEventListener("submit", function(event) {
+		event.preventDefault();
+		console.log(this.children[0].children[1].value, this.children[1].children[1].value);
+	});
 }());
